11b30ac99c6de7871431341cc15882788b9ed174
saturnhalkalari <burcudaskafa@gmail.com>
2023-06-01 09:17:44 +0300

    Initial Commit

---
+++ CryptoCrazyUI/CryptoCrazyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazyUIApp.swift
+//  CryptoCrazyUI
+//
+//  Created by Burcu Daşkafa on 1.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
