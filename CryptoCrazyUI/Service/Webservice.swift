0bbf2006a4e037ae7849fc15fcfc7b94413d98e4
Burcu Daşkafa <burcudaskafa@gmail.com>
2023-07-09 23:39:57 +0300

    files added

--- CryptoCrazyUI/Service/Webservice.swift
+++ CryptoCrazyUI/Service/Webservice.swift
@@ -6,3 +6,64 @@
 //
 
 import Foundation
+
+class Webservice {
+    
+    /*
+    //with async&await
+    func downloadCurrenciesAsync(url: URL) async throws -> [CryptoCurrency]{
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let currencies = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+        
+        return currencies ?? []
+    }*/
+    
+    
+    //converting escaping to async&await
+    func downloadCurrenciesContinuation(url: URL) async throws -> [CryptoCurrency]{
+       
+        try await withCheckedThrowingContinuation { continuation in
+            
+            downloadCurrencies(url: url) { result in
+                
+                switch result {
+                case .success(let cryptos):
+                    continuation.resume(returning: cryptos ?? [])
+                
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    
+    //with escaping
+    func downloadCurrencies(url: URL, completion: @escaping(Result<[CryptoCurrency]?,DownloaderError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(.failure(.badURL))
+            }
+            
+            guard let data = data , error == nil else{
+                return completion(.failure(.noData))
+            }
+            
+            guard let currencies = try? JSONDecoder().decode([CryptoCurrency].self, from: data) else{
+                return completion(.failure(.dataParseError))
+            }
+            
+            completion(.success(currencies))
+            }.resume()
+    }
+    
+}
+
+enum DownloaderError: Error {
+    case badURL
+    case noData
+    case dataParseError
+}
--- CryptoCrazyUI/View/MainView.swift
+++ CryptoCrazyUI/View/MainView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @ObservedObject var cryptoListViewModel : CryptoListViewModel
+    
+    init() {
+        self.cryptoListViewModel = CryptoListViewModel()
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(cryptoListViewModel.cryptoList, id:\.id) { crypto in
+                VStack{
+                    Text(crypto.currency)
+                        .font(.title3)
+                        .foregroundColor(.orange)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text(crypto.price)
+                        .font(.title2)
+                        .foregroundColor(.indigo)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }.toolbar(content: {
+                Button {
+                    //button clicked
+                    Task.init{
+                        cryptoListViewModel.cryptoList = []
+                        await cryptoListViewModel.downloadCrytosContinuation(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+                    }
+                }
+                label :{
+                    Text("Refresh")
+                }
+            })
+            
+            .navigationTitle("Crypto Crazy")
+        }.task {
+            await cryptoListViewModel.downloadCrytosContinuation(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+            
+            //await cryptoListViewModel.downloadCryptosAsync(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
         }
-        .padding()
+        
+        
+        /*
+        .onAppear {
+            cryptoListViewModel.downloadCryptos(url: URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!)
+                
+        }*/
     }
 }
 
