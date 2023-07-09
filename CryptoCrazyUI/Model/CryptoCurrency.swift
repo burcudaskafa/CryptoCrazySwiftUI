e6a2d1add9647b2b18deba4452efa82c021c9e9e
Burcu Daşkafa <burcudaskafa@gmail.com>
2023-07-09 23:39:31 +0300

    first commit

---
+++ CryptoCrazyUI/Model/CryptoCurrency.swift
@@ -0,0 +1,8 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCrazyUI
+//
+//  Created by Burcu Daşkafa on 1.06.2023.
+//
+
+import Foundation
---
+++ CryptoCrazyUI/Service/Webservice.swift
@@ -0,0 +1,8 @@
+//
+//  Webservice.swift
+//  CryptoCrazyUI
+//
+//  Created by Burcu Daşkafa on 1.06.2023.
+//
+
+import Foundation
